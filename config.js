39c21b708610983655ab4da2469b793a147d02aa
iisaraxsh-hue <ii.saraxsh@gmail.com>
2026-08-24 16:34:59 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://zegnyhsvintcntsdmfia.supabase.co";
+const SUPABASE_KEY = "sb_publishable_7srPiQjWTfnOCGb5kbuUjw_vD1rkbKN";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
